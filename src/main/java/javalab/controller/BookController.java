a264857ab9bb34e374687205d4e50aa3ab8ff6ff
Ilya <Ilya@DESKTOP-V9L0ANR>
2025-02-18 21:00:41 +0300

    Edited ReadME

--- src/main/java/javalab/controller/BookController.java
+++ src/main/java/javalab/controller/BookController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**Book controller class that handles book resource.*/
 @RestController
 @RequestMapping("/books")
 public class BookController {
