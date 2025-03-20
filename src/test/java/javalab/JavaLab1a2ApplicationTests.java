112924c4f9a6ad2531a139dfd674422bc78333e3
Ilya <Ilya@DESKTOP-V9L0ANR>
2025-03-20 11:22:34 +0300

    Added some cache tests

--- src/test/java/javalab/JavaLab1a2ApplicationTests.java
+++ src/test/java/javalab/JavaLab1a2ApplicationTests.java
@@ -1,14 +1,38 @@
 package javalab;
 
+import javalab.utility.InMemoryCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Map;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JavaLab1a2ApplicationTests {
 
 	@Test
-	void contextLoads() {
-		//Not done yet
+	void cacheDoesntDeleteOldEntries() {
+		Map<Integer, Integer> c1 = new InMemoryCache<>(2);
+
+		c1.put(1, 1);
+		c1.put(2, 2);
+		c1.put(3, 3);
+
+		assertThat(c1.containsKey(1)).isFalse();
 	}
 
+	@Test
+	void cacheEquals() {
+		Map<Integer, Integer> c1 = new InMemoryCache<>(2);
+		Map<Integer, Integer> c2 = new InMemoryCache<>(2);
+
+		c1.put(1, 1);
+		c2.put(2, 2);
+
+		assertThat(c1.equals(c2)).isFalse();
+
+		c2.clear();
+		c2.put(1, 1);
+
+		assertThat(c1.equals(c2)).isTrue();
+	}
 }
