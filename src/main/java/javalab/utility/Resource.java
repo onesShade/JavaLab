2ae60040afefaa72e885d13ceef08a92968021ac
Ilya <Ilya@DESKTOP-V9L0ANR>
2025-03-22 20:45:12 +0300

    First javadoc comment

--- src/main/java/javalab/utility/Resource.java
+++ src/main/java/javalab/utility/Resource.java
@@ -1,6 +1,11 @@
 package javalab.utility;
 
+/**Class representing different resource handling enums or similar.*/
 public class Resource {
+    /**Enum of load mode for resources.
+     * Default stands for loading from cache if possible & writing to it if not.
+     * Direct stands for loading from db and releasing from cache
+     * (because direct load is usually needed for data modification thus cache is outdated)*/
     public enum LoadMode {
         DEFAULT,
         DIRECT
