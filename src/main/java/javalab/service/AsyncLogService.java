55ba87acc6c86729e03b61391e41e5e890ea4aea
Ilya <Ilya@DESKTOP-V9L0ANR>
2025-05-11 15:46:18 +0300

    Attempting docker (at least works locally)

--- src/main/java/javalab/service/AsyncLogService.java
+++ src/main/java/javalab/service/AsyncLogService.java
@@ -39,7 +39,7 @@ public class AsyncLogService {
     public void createLogs(Long taskId, String date) {
         try {
             Thread.sleep(10000);
-        } catch (InterruptedException _) {
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
